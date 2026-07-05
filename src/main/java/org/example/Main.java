defcbbef442caa3ed54407d4b1614c0ee9c0e38f
Furkan Uzun <126771644+brsfurkanuzun@users.noreply.github.com>
2026-07-05 06:13:17 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,38 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+
+        return isBarking && (clock < 8 || clock >= 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19)
+                || (secondAge >= 13 && secondAge <= 19)
+                || (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int max = isSummer ? 45 : 35;
+
+        return temp >= 25 && temp <= max;
     }
 
     public static double area(double width, double height) {
-       
+       if(width<0||height<0)
+           return -1.0;
+       else
+           return width*height;
+
     }
 
     public static double area(double radius) {
-       
+       if(radius>0)
+           return radius*radius*Math.PI;
+       else
+           return -1.0;
+
     }
 }
